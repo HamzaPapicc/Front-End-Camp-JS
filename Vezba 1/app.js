dca785aa01416ac25f017b662b401bac7ef78b4d
Name Surname <hamza70.hp@gmail.com>
2021-10-24 14:30:14 +0200

    first commit

---
+++ Vezba 1/app.js
@@ -0,0 +1,23 @@
+//text="Please visit Microsoft!"
+
+//newText=text.replace("Microsoft", "Javascript")
+
+//console.log(text);
+//console.log(newText);
+
+//text="DoBaR dAn SvImA"
+
+//var1=text.toLowerCase()
+//var2=text.toUpperCase()
+
+//console.log(var1)
+//console.log(var2)
+
+// text="Hello";
+// console.log(text);
+
+// var1=text.concat(" ", "world");
+// console.log(var1);
+
+var1=['apple','banana','kiwi']
+console.log(var1)
